f934a124a8dd550a263d367fc2c3bd509b50e69d
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-09 15:51:52 +0530

    Update connection.py

--- D3VILBOT/modules/connection.py
+++ D3VILBOT/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import Mikobot.modules.sql.connection_sql as sql
-from Mikobot import dispatcher, DRAGONS, DEV_USERS
-from Mikobot.modules.helper_funcs import chat_status
-from Mikobot.modules.helper_funcs.alternate import send_message, typing_action
+import D3VILBOT.modules.sql.connection_sql as sql
+from D3VILBOT import dispatcher, DRAGONS, DEV_USERS
+from D3VILBOT.modules.helper_funcs import chat_status
+from D3VILBOT.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
