852089e2150eeb21751f435f4b95e973f393b9bc
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-10 11:49:57 +0530

    Update admin.py

--- D3VILBOT/modules/admin.py
+++ D3VILBOT/modules/admin.py
@@ -704,7 +704,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "Admin"
+__mod_name__ = "ADMIN 🌟"
 __command_list__ = [
     "adminlist",
     "admins",
